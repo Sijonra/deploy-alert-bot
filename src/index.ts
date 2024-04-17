476a9db94b7218a4a5850b3aeb1a85adfc977f28
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2024-04-17 18:08:55 +0300

    DAB-2: Add basic message

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,23 @@ const client = new Client({
 });
 
 client.login(config.BOT_TOKEN);
+
+client.once("ready", () => {
+  console.log("Bot is ready");
+
+  sendDeploymentMessage("Deploy Starts", "1229751423386910800");
+});
+
+function sendDeploymentMessage(startMessage, channelId) {
+  const channel = client.channels.cache.get(channelId);
+  if (channel) {
+    channel
+      .send(startMessage)
+      .then(() => console.log("Deployment message sent"))
+      .catch((error) =>
+        console.error("Error sending deployment message:", error)
+      );
+  } else {
+    console.error("Unable to find channel.");
+  }
+}
