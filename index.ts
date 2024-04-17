5e14233f3f8a8576ba950e2f3d88658cbefae7d8
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2024-04-17 17:03:13 +0300

    DAB-1: Add basic folders; Fix token issue

--- index.ts
+++
@@ -1,5 +0,0 @@
-const { Client, GatewayIntentBits } = require("discord.js");
-const config = require("./config.json");
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.login(config.BOT_TOKEN);
