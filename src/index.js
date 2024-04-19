3edb616dc907bd8b398ddfbdff1ee06d6b5f0315
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2024-04-19 15:50:42 +0300

    DAB-4: sendmessage function refactored to be async

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import { sendMessage } from "./bot";
+import { config } from "../creds.mjs";
+import { sendMessage } from "./bot.js";
+
+// const test = async () => {
+//   await sendMessage("super klass", config.DISCORD_CHANNEL_ID, config.BOT_TOKEN);
+//   console.log("after all");
+// };
+
+// test();
 
 export { sendMessage };
