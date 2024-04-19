2de13ee733f70b17339b131f0ac82bafca32cafc
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2024-04-19 16:04:17 +0300

    FIX: fix import modules

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+import { Client, GatewayIntentBits } from "discord.js";
 
 const client = new Client({
   intents: [
@@ -8,26 +8,44 @@ const client = new Client({
   ],
 });
 
-function sendMessageProcces(message, channelId) {
+async function sendMessageProcess(message, channelId) {
   const channel = client.channels.cache.get(channelId);
-  if (channel) {
-    channel
-      .send(message)
-      .then(() => console.log("Deployment message sent"))
-      .catch((error) =>
-        console.error("Error sending deployment message:", error)
-      );
-  } else {
-    console.error("Unable to find discord channel.");
-  }
+
+  return new Promise((resolve, reject) => {
+    if (channel) {
+      channel
+        .send(message)
+        .then(() => {
+          console.log("Deployment message sent");
+          resolve();
+        })
+        .catch((error) => {
+          console.error("Error sending deployment message:", error);
+          reject(error);
+        });
+    } else {
+      console.error("Unable to find discord channel.");
+      reject(new Error("Unable to find discord channel."));
+    }
+  });
 }
 
-export function sendMessage(message, channelId, BOT_TOKEN) {
-  client.login(BOT_TOKEN);
+export async function sendMessage(message, channelId, BOT_TOKEN) {
+  await client.login(BOT_TOKEN);
 
-  client.once("ready", () => {
-    console.log("Bot is ready");
+  return new Promise((resolve, reject) => {
+    client.once("ready", async () => {
+      console.log("Bot is ready");
 
-    sendMessageProcces(message, channelId);
+      try {
+        await sendMessageProcess(message, channelId);
+        resolve();
+        process.exit();
+      } catch (error) {
+        console.error("Error sending deployment message:", error);
+        reject(error);
+        process.exit(1);
+      }
+    });
   });
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-// import { config } from "../creds.mjs";
+import { config } from "../.creds.mjs";
 import { sendMessage } from "./bot.js";
 
-// const test = async () => {
-//   await sendMessage("super klass", config.DISCORD_CHANNEL_ID, config.BOT_TOKEN);
-//   console.log("after all");
-// };
+const test = async () => {
+  await sendMessage("super klass", config.DISCORD_CHANNEL_ID, config.BOT_TOKEN);
+  console.log("after all");
+};
 
-// test();
+test();
 
 export { sendMessage };
