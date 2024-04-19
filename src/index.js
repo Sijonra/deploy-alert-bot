23b074ac16b45f231ef30314f60a48dc141129cd
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2024-04-19 16:08:24 +0300

    FIX: fixed index file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import { config } from "../.creds.mjs";
 import { sendMessage } from "./bot.js";
-
-const test = async () => {
-  await sendMessage("super klass", config.DISCORD_CHANNEL_ID, config.BOT_TOKEN);
-  console.log("after all");
-};
-
-test();
 
 export { sendMessage };
